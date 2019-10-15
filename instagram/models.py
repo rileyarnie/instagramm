775ab52f6ce059d2c27b498e9b83c2b3641627f0
Arnold Kibira <arnold.ril3y@gmail.com>
2019-10-15 23:12:57 +0300

    edit uimage model fields

--- instagram/models.py
+++ instagram/models.py
@@ -15,3 +15,28 @@ class Profile(models.Model):
     
     def __str__(self):
         return f'{self.user.username}\'s Profile'
+
+    def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
+        img = PIL.Image.open(self.pic.path)
+
+        if img.height > 300 or img.width >300:
+            output_size = (300,300)
+            img.thumbnail(output_size)
+            img.save(self.pic.path)
+
+class Image(models.Model):
+    image_post = models.ImageField( upload_to='posts')
+    image_caption = models.TextField()
+    image_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='likes', blank=True)
+
+    def __str__(self):
+        return self.image_caption
+
+    def total_likes(self):
+        return self.likes.count()
+
+    def get_absolute_url(self):
+        return reverse('home')
