89a0c93a5b75af1f7b2db001ae68485d1163e398
Arnold Kibira <arnold.ril3y@gmail.com>
2019-10-16 12:08:19 +0300

    create an ImageCreateView which allows the user to create a new post which includes the image and caption.

--- instagram/views.py
+++ instagram/views.py
@@ -1,15 +1,96 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.views.generic import ListView, CreateView, DeleteView, DetailView
+from .models import Image, Profile
+from django.contrib.auth.models import User
+from .forms import UserRegisterForm, ProfileUpdateForm, UserUpdateForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+def home(request):
+    return render(request, 'instagram/home.html')
+
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data.get('username')
-            messages.success
+            form.save()
+            username=form.cleaned_data.get('username')
+            messages.success(request, f'Account created! Log In Now!')
+            return redirect('login')
+
     else:
-        form = UserCreationForm()
-    return render(request, 'users/register.html', {'form':form})
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+
+class ImageListView(ListView):
+    model = Image 
+    context_object_name = 'images'
+    ordering = ['-date_posted']
+
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST,instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Account Updated!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context={
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'users/profile.html', context)
+
+
+# class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    
+#     model = Post
+#     fields = ['title', 'content']
+
+#     def form_valid(self,form):
+#         form.instance.author = self.request.user
+#         return super().form_valid(form)
+
+#     def test_func(self):
+#         post = self.get_object()
+#         if self.request.user == post.author:
+#             return True
+#         return False
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST,instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Account Updated!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context={
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'users/profile.html', context)
+
+
+class ImageCreateView(LoginRequiredMixin, CreateView):
+    model = Image
+    fields = ['image_post', 'image_caption'] 
+
+    def form_valid(self, form):
+        form.instance.image_by = self.request.user
+        return super().form_valid(form)
