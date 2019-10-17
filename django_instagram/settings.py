9a2197a76770a9e6fd0ebba9c42291b528953217
Arnold Kibira <arnold.ril3y@gmail.com>
2019-10-17 20:45:53 +0300

    link the static files url properly to enable them to load on heroku

--- django_instagram/settings.py
+++ django_instagram/settings.py
@@ -121,6 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIR =[
+    os.path.join(BASE_DIR,'static'),
+]
+
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL= '/media/'
 
